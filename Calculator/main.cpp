135fd16e7a8da5c1a1e713dd3993b5e8fcd6629e
Fev1L <demchvlad17@gmail.com>
2025-10-28 13:52:14 +0100

    Initial Commit

---
+++ Calculator/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Calculator
+//
+//  Created by Fev1L on 28.10.2025.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
